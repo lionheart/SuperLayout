771513941b07f6b1b6af5b67ef85d359eb57b192
Dan Loewenherz <dan@lionheartsw.com>
2017-01-23 19:56:55 -0600

    add tests

--- Tests/SuperLayoutTests/SuperLayoutTests.swift
+++ Tests/SuperLayoutTests/SuperLayoutTests.swift
@@ -2,16 +2,34 @@ import XCTest
 @testable import SuperLayout
 
 class SuperLayoutTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(SuperLayout().text, "Hello, World!")
+    var viewA: UIView!
+    var viewB: UIView!
+
+    override func setUp() {
+        let viewA = UIView()
+        viewA.translatesAutoresizingMaskIntoConstraints = false
+
+        let viewB = UIView()
+        viewB.translatesAutoresizingMaskIntoConstraints = false
+
+        let container = UIView()
+        container.addSubview(viewA)
+        container.addSubview(viewB)
     }
 
+    func testConstraint1() {
+        let constraint = viewA.rightAnchor ~~ viewB.leftAnchor
+        XCTAssert(constraint.constant == 0)
+    }
+
+    func testConstraint2() {
+        let constraint = viewA.rightAnchor ~~ viewB.leftAnchor
+        XCTAssert(constraint.multiplier == 1)
+    }
 
     static var allTests : [(String, (SuperLayoutTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testExample", testConstraint1),
         ]
     }
 }
