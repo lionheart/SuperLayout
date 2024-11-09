c09f7d1e459185345d2d0fce6e606f71ca14269b
Dan Loewenherz <dan@lionheartsw.com>
2024-11-09 09:54:55 -0600

    migrate to spm (#7)

--- Package.swift
+++ Package.swift
@@ -1,5 +1,25 @@
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
-    name: "SuperLayout"
+    name: "SuperLayout",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "SuperLayout",
+            targets: ["SuperLayout"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "SuperLayout",
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "SuperLayoutTests",
+            dependencies: ["SuperLayout"]
+        ),
+    ]
 )
