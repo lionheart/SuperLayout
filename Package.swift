5d52d5e8d59568ea6ae6400320123d48ad709b9b
Dan Loewenherz <dan@lionheartsw.com>
2024-11-09 07:23:07 -0600

    add SPM

--- Package.swift
+++ Package.swift
@@ -1,5 +1,30 @@
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
-    name: "SuperLayout"
+    name: "SuperLayout",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "SuperLayout",
+            targets: ["SuperLayoutCore"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SuperLayoutCore",
+            path: "Sources",
+            sources: ["*"],
+            swiftSettings: [
+                .define("SWIFT_VERSION", to: "5.0")
+            ]
+        ),
+        .testTarget(
+            name: "SuperLayoutTests",
+            dependencies: ["SuperLayoutCore"]
+        ),
+    ]
 )
